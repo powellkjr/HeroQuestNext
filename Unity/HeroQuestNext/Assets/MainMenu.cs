5e23d3fd6ab7215c8d2b663cec8b7ec549e0ccb5
Powell.Kelvin <Powell.Kelvin@gmail.com>
2020-10-04 00:11:00 -0400

    Added Fonts and backgrounds Made Scenes for Main menu and main game

---
+++ Unity/HeroQuestNext/Assets/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public enum Scenes
+    {
+        MainMenuScene,
+        MainGameScene,
+    };
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene(Scenes.MainGameScene.ToString());
+        Debug.Log("Play Clicked, Switching Scene to MainGameScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit Clicked, Quitting");
+    }
+}
---
+++ Unity/HeroQuestNext/Assets/PauseMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene(MainMenu.Scenes.MainMenuScene.ToString());
+        Debug.Log("Main Menu Clicked, Switching Scene to MainMenuScene");
+    }
+}
